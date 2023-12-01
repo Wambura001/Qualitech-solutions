8061c5a0970d212b7ac753d53d7892ea97401eae
wambura <meshack.sirincha@student.moringaschool.com>
2023-12-01 23:16:14 +0900

    Add react folder

---
+++ on-react/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Body from "./Body";
+
+
+function App () {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+}
+
+
+export default App;
---
+++ on-react/src/Body.jsx
@@ -0,0 +1,26 @@
+function Body(){
+  return(
+    <>
+      <div className="1">
+        <h1>We build</h1>
+        <h2>And Manintain</h2>
+        <h3>Web applications</h3>
+        <img src="" alt=""/>
+        {/* call to action */}
+      </div>
+      <div className="2">
+        <h2>What to Expect</h2>
+        {/* call to action */}
+      </div>
+      <div className="3">
+        <h2>About us</h2>
+        {/* call to action */}
+      </div>
+      <div className="4">
+        <h2>Testimonials</h2>
+      </div>
+    </>
+  )
+}
+
+export default Body
